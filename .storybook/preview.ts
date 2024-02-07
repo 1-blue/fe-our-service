7ccfb243c0ad0579c4fbaaa4378dcd15df24605e
1-blue <1-blue98@naver.com>
2024-02-07 20:07:19 +0900

    초기 세팅
    1. Next.js 14 ( app )
    2. TypeScript
    3. TailwindCss
    4. Storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,20 @@
+import "../src/css/tailwindcss.css";
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: "default",
+      values: [{ name: "default", value: "#000505" }],
+    },
+  },
+};
+
+export default preview;
