cde74dd0e9353e629a4f1511872c5a267efa4810
1-blue <1-blue98@naver.com>
2024-04-13 14:40:10 +0900

    Feat: 회원가입 로직 구현
    1. 이메일
    2. 이메일 인증
    3. 비밀번호
    4. 닉네임
    5. 프로필 이미지, 휴대폰 번호

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+import SignUpForm from "#/app/_components/signup/SignUpForm";
+
+const Page: NextPage = () => {
+  return <SignUpForm />;
+};
+
+export default Page;
